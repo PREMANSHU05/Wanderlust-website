972728e778556303bb5f8751179f0a6f29621c36
PREMANSHU05 <premanshupadwal@gmail.com>
2026-03-21 19:32:30 +0530

    adding image url

--- models/listing.js
+++ models/listing.js
@@ -19,7 +19,8 @@ const listeningSchema = new Schema({
         },
         url:{
             type:String,
-            default:"https://twocontinents.com/attraction/img-worlds-of-adventure",
+            default:"https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+
         }
     },
     price:Number,
