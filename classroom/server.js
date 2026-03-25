b1422119a7e2f73bad44adb385597fbc441a5829
PREMANSHU05 <premanshupadwal@gmail.com>
2026-03-25 21:07:41 +0530

    begin cookies

--- classroom/server.js
+++ classroom/server.js
@@ -2,10 +2,33 @@ const express = require("express");
 const app = express();
 const users = require("./routes/user.js");
 const posts = require("./routes/post.js");
+//Cookie parser
+const cookieParser = require("cookie-parser");
 
-
-
+app.use(cookieParser("secretcode"));
+//send signed cookies
+app.get("/getsignedcookie",(req,res)=>{
+    res.cookie("made-in","india",{signed:true});
+    res.send("signeed cookie send");
+})
+//verify signed cookies
+app.get("verify",(req,res)=>{
+    console.log(req.signedCookies);
+    res.send("verified");
+})
+//send cookie to server
+app.get("/getcookies",(req,res)=>{
+    res.cookie("greet","hello");
+    res.cookie("madein","india");
+    res.send("sent you some cookies");
+})
+app.get("/greet",(req,res)=>{
+    let {name = "anonymous" } = req.cookies;
+    res.send(`hi ,${name}`);
+});
+// cookie parse 
 app.get("/",(req,res)=>{
+    console.dir(req.cookies);
     res.send("hi , i am root !")
 });
 
