16f73edd29d8f1781b2ddb408de4b97b9bfb95b3
PREMANSHU05 <premanshupadwal@gmail.com>
2026-03-26 20:19:38 +0530

    begin cookies

--- classroom/server.js
+++ classroom/server.js
@@ -2,63 +2,44 @@ const express = require("express");
 const app = express();
 const users = require("./routes/user.js");
 const posts = require("./routes/post.js");
-//Cookie parser
+// Cookie parser
 const cookieParser = require("cookie-parser");
-
-app.use(cookieParser("secretcode"));
-//send signed cookies
-app.get("/getsignedcookie",(req,res)=>{
-    res.cookie("made-in","india",{signed:true});
-    res.send("signeed cookie send");
-})
-//verify signed cookies
-app.get("verify",(req,res)=>{
-    console.log(req.signedCookies);
-    res.send("verified");
-})
-//send cookie to server
-app.get("/getcookies",(req,res)=>{
-    res.cookie("greet","hello");
-    res.cookie("madein","india");
-    res.send("sent you some cookies");
-})
-app.get("/greet",(req,res)=>{
-    let {name = "anonymous" } = req.cookies;
-    res.send(`hi ,${name}`);
-});
+const session = require("express-session");
+// app.use(cookieParser("secretcode"));
+// send signed cookies
+// app.get("/getsignedcookie",(req,res)=>{
+//     res.cookie("made-in","india",{signed:true});
+//     res.send("signeed cookie send");
+// })
+// verify signed cookies
+// app.get("verify",(req,res)=>{
+//     console.log(req.signedCookies);
+//     res.send("verified");
+// })
+// send cookie to server
+// app.get("/getcookies",(req,res)=>{
+//     res.cookie("greet","hello");
+//     res.cookie("madein","india");
+//     res.send("sent you some cookies");
+// })
+// app.get("/greet",(req,res)=>{
+//     let {name = "anonymous" } = req.cookies;
+//     res.send(`hi ,${name}`);
+// });
 // cookie parse 
-app.get("/",(req,res)=>{
-    console.dir(req.cookies);
-    res.send("hi , i am root !")
-});
+// app.get("/",(req,res)=>{
+//     console.dir(req.cookies);
+//     res.send("hi , i am root !")
+// });
 
-app.use("/users",users);
-app.use("/posts",posts);
+// app.use("/users",users);
+// app.use("/posts",posts);
 
-//posts
-//index 
+app.use(session({secret: "mysupersecretstring"}));
 
-app.get("/posts",(req,res)=>{
-    res.send("GET for posts");
-});
-//show 
-
-app.get("/posts/:id",(req,res)=>{
-    res.send("GET for posts id");
-});
-
-//POST 
-
-app.post("/posts",(req,res)=>{
-    res.send("POST for post");
-});
-
-//Delete 
-
-app.delete("/posts/:id",(req,res)=>{
-    res.send("Delete for posts id");
-});
-
+app.get("/test",(req,res)=>{
+    res.send("test succesful");
+})
 
 app.listen(3000,()=>{
     console.log("server is listening to 3000");
